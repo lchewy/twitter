f9876b307864c60bfced5b0a923d3e9737ac9d5f
Leslie Chiu <leslie@Leslies-MacBook-Pro.local>
2017-08-01 11:20:41 -0400

    done middleware

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const app = express();
+const volley = require('volleyball')
 
 app.listen(3000, function(){
     console.log("server listening")
 })
 
-app.get("/", function(req, res){
+app.use(volley)
+
+app.get('/news', function(req, res, next){
+    res.send('NEWS!!!!');
+    console.log(req.method, req.url, "");
+    next();
+})
+
+app.get("/", function(req, res, next){
     res.send("Welcome!")
+    next();
+})
+
+app.get("/is-anybody-in-there", function(req, res, next){
+    res.send("blah")
+    console.log(req.method, req.url,"")
+    next();
+})
+
+app.get("/modernism", function(req, res, next){
+    // console.log(req.method, req.url, "")
+    res.send('<h1>MODERNISM</h1>')
+    
+    // next();
+})
+
+app.post("/modernism", function(req, res, next){
+    // console.log(req.method, req.url, "")
+    console.log(req.method, req.url, "")
+    
+    // next();
 })

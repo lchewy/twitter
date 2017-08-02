f18548e1a8fc285ed57e635d78d179d2a0e95360
Leslie Chiu <leslie@Leslies-MacBook-Pro.local>
2017-08-01 20:02:42 -0400

    add single user

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
-const volley = require('volleyball')
+const volley = require('volleyball');
+const nunjucks = require('nunjucks');
+const tweetBank = require('./tweetBank.js')
+const routes = require('./routes');
+
+app.use('/', routes);
+app.use(express.static('public'))
+
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+
+ const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 app.listen(3000, function(){
     console.log("server listening")
@@ -8,33 +21,69 @@ app.listen(3000, function(){
 
 app.use(volley)
 
-app.get('/news', function(req, res, next){
-    res.send('NEWS!!!!');
-    console.log(req.method, req.url, "");
-    next();
+app.get('/', function(req, res){
+    res.sendFile('stylesheets/style.css')
 })
 
-app.get("/", function(req, res, next){
-    res.send("Welcome!")
-    next();
-})
 
-app.get("/is-anybody-in-there", function(req, res, next){
-    res.send("blah")
-    console.log(req.method, req.url,"")
-    next();
-})
 
-app.get("/modernism", function(req, res, next){
-    // console.log(req.method, req.url, "")
-    res.send('<h1>MODERNISM</h1>')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/news', function(req, res, next){
+//     res.render("index.html")
+//     console.log(req.method, req.url, "");
+//     // next();
+// })
+
+// app.get("/", function(req, res, next){
+//     // res.send("Welcome!")
+   
+//     res.render( 'index', {title: 'Hall of Fame', people: people} );
+//     // next();
+// })
+
+// app.get("/is-anybody-in-there", function(req, res, next){
+//     res.send("blah")
+//     console.log(req.method, req.url,"")
+//     // next();
+// })
+
+// app.get("/modernism", function(req, res, next){
+//     // console.log(req.method, req.url, "")
+//     res.send('<h1>MODERNISM</h1>')
     
-    // next();
-})
+//     // next();
+// })
 
-app.post("/modernism", function(req, res, next){
-    // console.log(req.method, req.url, "")
-    console.log(req.method, req.url, "")
+// app.post("/modernism", function(req, res, next){
+//     // console.log(req.method, req.url, "")
+//     console.log(req.method, req.url, "")
     
-    // next();
-})
+//     // next();
+// })
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,19 @@
 const express = require('express');
-const app = express();
+// const app = express();
+const router = express.Router();
+const tweetBank = require('../tweetBank.js')
 
+router.get('/', function (req, res) {
+  let tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+router.get('/users/:name', function(req, res) {
+  
+  var name = req.params.name;
+  //et tweets = tweetBank.list();
+  tweets = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: tweets, name: name} );
+});
+
+module.exports = router;
